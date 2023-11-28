b63de9d2c02f0acd633fca9794850c7f7a9d3ddb
Md <md1040582@gmail.com>
2023-11-28 21:03:46 +0600

    font size change

--- client/src/components/View/ViewForm.js
+++ client/src/components/View/ViewForm.js
@@ -18,13 +18,13 @@ const ViewForm = () => {
             sectorData?.form.map((item,index)=>{
               return(
                 <div key={index} className='w-fit h-fit bg-white shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] rounded-md m-3 cursor-pointer'>
-                 <p className='text-lg font-serif text-left px-4 font-medium'> <strong>Name: </strong>{item?.name}</p>
-                 <p className='text-left px-4 font-serif font-semibold text-lg'>Sectors:</p>
+                 <p className='text-[16px] font-serif text-left px-4 font-medium'> <strong>Name: </strong>{item?.name}</p>
+                 <p className='text-left px-4 font-serif font-semibold text-[16px]'>Sectors:</p>
                  <div className='flex items-center flex-row flex-wrap px-3 py-1 mb-2'>
                   {
                     item?.sectors?.map((item,index)=>(
                       <div key={index} className='m-1 shadow rounded-lg'>
-                        <p className=' p-1 text-[16px] font-medium '>{item}</p>
+                        <p className=' p-1 text-[15px] font-medium '>{item}</p>
                       </div>
                     ))
                   }
