98f78a4b0e894073fa2a2d1890b5afe0d5fe340b
Md <md1040582@gmail.com>
2023-11-28 16:32:55 +0600

    ...

--- client/src/components/form/HeaderText.js
+++ client/src/components/form/HeaderText.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HeaderText = () => {
   return (
-    <div className='bg-gray-100 px-6 py-4 rounded-lg'>
-    <p className='font-sans font-medium text-lg text-center text-gray-800'>
+    <div className='bg-gray-100 px-6 py-3 rounded-lg'>
+    <p className='font-sans font-medium text-[16px] text-center text-gray-800'>
     Please enter your name and pick the Sectors you are currently involved in.
     </p>
 </div>
